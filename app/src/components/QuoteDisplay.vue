a888e565843b0454580bc2442ac5dcaf0a955877
fox <fernandodiego.sn@gmail.com>
2023-09-21 20:19:50 -0300

    Quotes are on the screen, and randomize is functional, no style for now

---
+++ app/src/components/QuoteDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-text>
+        <v-subtitle v-if="quote">{{ quote.quote }}</v-subtitle>
+        <v-caption v-if="quote">{{ quote.author }}</v-caption>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    quote: Object, // The quote to display
+  },
+};
+</script>
---
+++ app/src/components/RandomQuoteButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn @click="getRandomQuote" color="primary">Get Random Quote</v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    getRandomQuote() {
+      // Emit an event to request a random quote
+      this.$emit("get-random-quote");
+    },
+  },
+};
+</script>
